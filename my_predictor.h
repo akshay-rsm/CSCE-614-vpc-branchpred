1d1659954cd366a3153b7e8a449a777b08f0115a
akshay-ramuhally <akshay.ramuhally@tamu.edu>
2017-10-30 23:08:14 -0500

    Update my_predictor.h

--- my_predictor.h
+++ my_predictor.h
@@ -1,5 +1,5 @@
-// my_predictor.h
-// simple direct-mapped branch target buffer for indirect branch prediction.
+/***my_predictor.h*****/
+
 #include <map>
 #include <assert.h>
 #include <cmath>
@@ -148,7 +148,7 @@ class perceptron_table //the map which keeps track of the various nodes and thei
     perceptron_list *p_list; //pointer to linked list  of perceptron nodes
     std::map<unsigned int,perceptron_node*> p_map; // unordered map to keep track of addresse and perceptron nodes 
     
-    perceptron_table(unsigned int cap)
+    perceptron_table(unsigned int cap) // constructor
     {
         this->capacity = cap;
         size=0;
@@ -156,19 +156,19 @@ class perceptron_table //the map which keeps track of the various nodes and thei
         p_map = map<unsigned int, perceptron_node*>();
     }
     
-    perceptron_node* get(unsigned int vpca)
+    perceptron_node* get(unsigned int vpca) //returns the perceptron node associated with a particular vpca 
     {
         if(p_map.find(vpca) == p_map.end()){
-            return NULL;
+            return NULL;		//return NULL if node doesn't exist
         }
         
-        p_list->move_node_to_head(p_map[vpca]);
+        p_list->move_node_to_head(p_map[vpca]); // this node just became the most used
         return p_map[vpca];
     }
     
     void put(unsigned int vpca, int* vec)
     {
-        if(vec == NULL)
+        if(vec == NULL)				//this means that the node hasn't been created yet, so we get a new one 
         {
             if(size == capacity)
             {
@@ -179,10 +179,10 @@ class perceptron_table //the map which keeps track of the various nodes and thei
             }
             perceptron_node* perp = p_list->add_to_head(vpca);
             size++;
-            p_map[vpca] = perp;
+            p_map[vpca] = perp; //map the new node to the address
             return;
         }
-        else if(p_map.find(vpca) != p_map.end())
+        else if(p_map.find(vpca) != p_map.end()) //the node already exists, enter new values into existing perceptron
         {
             for(int i=0;i<PERCEPTRON_SIZE;i++)
             {
@@ -199,86 +199,84 @@ class perceptron_table //the map which keeps track of the various nodes and thei
 
 };
 
-void update_bias(perceptron_node* p_node,int taken)
-{
-    int new_bias = p_node->bias + taken;
-    if(abs(new_bias) < 128 )
-    {
-        p_node->bias = new_bias;
-    }
-}
-void update_weights(perceptron_node* p_node,int taken, unsigned int vghr)
-{
-    int new_weight;
-    for(int i=0;i<PERCEPTRON_SIZE;i++)
-    {
-        new_weight = p_node->perceptron[i] + (taken * get_history_bit(vghr, i));
-        if(abs(new_weight) < 128)
-        {
-            p_node->perceptron[i] = new_weight;
-        }
-    }
-}
-class perceptron_predictor
+class perceptron_predictor //the predictor class
 {
     public:
-    perceptron_table* p_table;
-    static const int theta=40;
-    unsigned int size_of_table;
+    perceptron_table* p_table; // perceptron table
+    static const int theta=40; // learning threshold
+    unsigned int size_of_table; //the number of perceptrons
     perceptron_predictor(unsigned int size)
     {
         this->size_of_table = size;
         p_table = new perceptron_table(size);
     }
-    int compute(unsigned int vpca, unsigned int vghr)
-    {
-        perceptron_node* p_node = p_table->get(vpca);
+    int compute(unsigned int vpca, unsigned int vghr) // the computation function, returns an integer result
+    {							// y = dotproduct(w, history) + bias
+        perceptron_node* p_node = p_table->get(vpca); //get the relevant perceptron node
         int y;
-        if(p_node == NULL)
+        if(p_node == NULL)			     //if node doesn't exist
         {
             p_table->put(vpca,NULL);
             p_node = p_table->get(vpca);
         }
-        y = p_node->bias;
+        y = p_node->bias;			//add bias(w0)
         for(int i = 0;i < PERCEPTRON_SIZE;i++)
         {
             y+=(p_node->perceptron[i] * get_history_bit(vghr,i)); 
         }
-        return y;
+        return y; //result of computation
     }
     
     void update(unsigned int vpca, int result, int taken, unsigned int vghr)
     {
-        perceptron_node* p_node = p_table->get(vpca);
-       	if(p_node == NULL)
+        perceptron_node* p_node = p_table->get(vpca); //get the required node
+       	if(p_node == NULL)			//if it doesn't exist
 	{
 		p_table->put(vpca,NULL);
 		p_node = p_table->get(vpca);
 	}
 	 
-        if(sign(result) != taken || abs(result) <= theta)
+        if(sign(result) != taken || abs(result) <= theta) //if the result of computation and the actual outcome don't match or if the result is lesser than the learning threshold 
         {
-            update_bias(p_node,taken);
+            update_bias(p_node,taken);		
             update_weights(p_node,taken,vghr);
         }
     }
+    void update_bias(perceptron_node* p_node,int taken)
+    {
+    	int new_bias = p_node->bias + taken;
+    	if(new_bias < 7 && new_bias > 0 )  //each weight is a saturating three-bit counter
+    	{
+       		p_node->bias = new_bias;
+    	}
+    }
+    void update_weights(perceptron_node* p_node,int taken, unsigned int vghr)
+    {
+    	int new_weight;
+    	for(int i=0;i<PERCEPTRON_SIZE;i++)
+    	{
+        	new_weight = p_node->perceptron[i] + (taken * get_history_bit(vghr, i)); 
+        	if(new_weight < 7 && new_weight > 0)
+        	{
+            		p_node->perceptron[i] = new_weight;
+        	}
+    	}
+    }
     
 };
-class btb_node
+class btb_node					//basic node of Branch Target Buffer
 {
     public:
-    unsigned int vpca;
-    unsigned int target;
+    unsigned int vpca;				//branch address
+    unsigned int target;			//predicted target
     btb_node* prev,*next;
     btb_node(unsigned int vpca, unsigned int address ): vpca(vpca), target(address),prev(NULL), next(NULL){}  
 };
-class btb_list
+class btb_list					//Doubly linked list of BTB nodes
 {
     
     public:
-    
     btb_node *front,*rear;
-    
     btb_list(): front(NULL), rear(NULL){}
     
     bool no_entries()
@@ -361,32 +359,31 @@ class btb_list
         return front;
     }
 };
-class btb
+class btb //BTB class. Uses unordered map and linked list to implement an LRU cache.
 {
     public:
     unsigned  size, capacity;
-    btb_list *b_list;
-    map<unsigned int,btb_node*> b_map;
+    btb_list *b_list; //linked list
+    map<unsigned int,btb_node*> b_map; //the unordered map
     
     btb(unsigned int cap)
     {
-        this->capacity = cap;
+        this->capacity = cap; //set capacity of BTB ( no. of entries)
         size=0;
         b_list = new btb_list();
         b_map = map<unsigned int, btb_node*>();
     }
     
-    unsigned int get(unsigned int vpca)
+    unsigned int get(unsigned int vpca)   //get the target address
     {
         if(b_map.find(vpca) == b_map.end()){
             return 0;
         }
-        //unsigned int* p_vector = p_map[vpca]->perceptron;
         b_list->move_node_to_head(b_map[vpca]);
         return b_map[vpca]->target;
     }
     
-    void put(unsigned int vpca, unsigned int address)
+    void put(unsigned int vpca, unsigned int address) //insert value into BTB
     {
         if(b_map.find(vpca) != b_map.end())
         {
@@ -419,21 +416,21 @@ public:
 class my_predictor : public branch_predictor 
 {
 public:
-#define HISTORY_LENGTH	20
-#define NO_OF_PERCEPTRONS 64
-#define SIZE_OF_BTB 64
-#define MAX_ITER 16
+#define HISTORY_LENGTH	20 //how far back into history are we looking?
+#define NO_OF_PERCEPTRONS 1024 //the number of perceptrons
+#define SIZE_OF_BTB 1024 //size of the BTB
+#define MAX_ITER 8  //the number of iterations of VPC
 	my_update u;
 	branch_info bi;
-	unsigned int ghr;	
+	perceptron_predictor* p_pred; //predictor object
+	btb* btb_1; //BTB object
+	unsigned int ghr;	//global history register
 	unsigned int hashvals[MAX_ITER];
 	int result_of_perceptron[MAX_ITER];
-	unsigned int pred_target;
-	int pred_iter;
-	perceptron_predictor* p_pred;
-	btb* btb_1;
-
-	my_predictor (void) : ghr(0),hashvals {1392,4695,2615,5174,2648,2810,7302,5494,6865,3519,6155,3489,5263,6911,5501,2558}
+	unsigned int pred_target; //the predicted target of BTB
+	int pred_iter; // the iteration at which prediction is made
+	
+	my_predictor (void) : ghr(0),hashvals {1392,4695,2615,5174,2648,2810,7302,5494}//,6865,3519,6155,3489,5263,6911,5501,2558}
 	{ 
 		p_pred = new perceptron_predictor(NO_OF_PERCEPTRONS);
 		btb_1 = new btb(SIZE_OF_BTB);
@@ -441,40 +438,40 @@ public:
 
 	branch_update *predict (branch_info & b) 
 	{
-	/* 	1)Create two objects of type Perceptron_Tab and BTB
-	   	2)Algorithm 1 of VPC
+	/* 	
+	   	Algorithm 1 of VPC
 	*/
 		bi = b;
-		if(b.br_flags & BR_INDIRECT)
+		if(b.br_flags & BR_INDIRECT)   //for indirect branches only
 		{
-			int i=0;
-			unsigned int vghr = ghr;
-			unsigned int vpca = b.address;
-			bool done = false;
+			int i=0;		//iterator
+			unsigned int vghr = ghr;	//virtual history i.e history of branch
+			unsigned int vpca = b.address;	//initally vpca is the PC itself
+			bool done = false;		
 			
 			do
 			{
-				pred_target = btb_1->get(vpca);
-				result_of_perceptron[i] = p_pred->compute(vpca,vghr);
-				bool direction = result_of_perceptron[i]>=0 ? true:false;
+				pred_target = btb_1->get(vpca);		//get the BTB prediction
+				result_of_perceptron[i] = p_pred->compute(vpca,vghr);	//compute the result and store in an array
+				bool direction = result_of_perceptron[i]>=0 ? true:false; //will the branch be taken?
 				if(pred_target && (direction == true))
 				{
-					u.target_prediction(pred_target);
+					u.target_prediction(pred_target);	//found target!
 					done = true;
 					i = i-1;
 				}
-				else if((!pred_target) || (i >= (MAX_ITER - 1)))
+				else if((!pred_target) || (i >= (MAX_ITER - 1)))	//no hit in BTB or VPC has run too many times
 				{
 					u.target_prediction(0);
 					done = true;
 					i=i-1;
 				}
-			vpca = hash_func(b.address, hashvals[i]);
-			vghr = vghr << 1;
+			vpca = hash_func(b.address, hashvals[i]);		//get new vpca
+			vghr = vghr << 1;					//shift virtual history
 			i++;	 
 			}while(!done);
 			
-			pred_iter = i; 
+			pred_iter = i; 						//get the iteration at which prediction was made
 		}
 		if(b.br_flags & BR_CONDITIONAL)
 		{
@@ -486,28 +483,30 @@ public:
 
 	void update (branch_update *u, bool taken, unsigned int target)
 	{
+	/* algorithm 2 & 3 */
 		bool correct_prediction = false;
 		if(bi.br_flags & BR_INDIRECT)
 		{
-			if(u->target_prediction() == target)
+			if(u->target_prediction() == target) 			//did we make the right prediction?
 			{
 				correct_prediction = true;
 			}
 			if(correct_prediction)
 			{
+			/* Algorithm 2*/
 				int i=0;
 				unsigned int vpca = bi.address;
 				unsigned int vghr = ghr; 	
 				do
 				{
-					if(i == pred_iter)
+					if(i == pred_iter)			
 					{
-						btb_1->put(vpca,target);
-						p_pred->update(vpca,result_of_perceptron[i],1,vghr);
+						btb_1->put(vpca,target);		//update the BTB
+						p_pred->update(vpca,result_of_perceptron[i],1,vghr);	//update the perceptron
 					}
 					else
 					{
-						p_pred->update(vpca,result_of_perceptron[i],-1,vghr);
+						p_pred->update(vpca,result_of_perceptron[i],-1,vghr);	//update the perceptron with not-taken
 					}
 					vpca = hash_func(bi.address, hashvals[i]);
 					vghr = vghr << 1;
@@ -516,49 +515,42 @@ public:
 			}
  			else
 			{
+			/* Algorithm 3 */
 				int i=0;
 				unsigned int vpca = bi.address;
 				unsigned int vghr = ghr;
-				bool found_target = false;
+				bool found_target = false;		//check for the right target 
 				do
 				{	
 					pred_target = btb_1->get(vpca);
-					//printf("%u",pred_target);
-					if(pred_target == target)
+					if(pred_target == target) 	//found the target
 					{
-						p_pred->update(vpca,result_of_perceptron[i],+1,vghr);
+						p_pred->update(vpca,result_of_perceptron[i],+1,vghr); //this branch was taken
 						btb_1->put(vpca,target);
 						found_target = true;
 					}
 					else if(pred_target)
 					{
-						p_pred->update(vpca,result_of_perceptron[i],-1,vghr);
+						p_pred->update(vpca,result_of_perceptron[i],-1,vghr); //this branch was not taken
 					}
 					vpca = hash_func(bi.address, hashvals[i]);
-					vghr = vghr << 1; 										 i++;
+					vghr = vghr << 1; 									
+					i++;
 				}while((i<=MAX_ITER-1) && (!found_target));
-				if(!found_target)
+				if(!found_target)    				//if target is still not found
 				{
-					int take;
-					if(taken)
-					{
-						take = 1;
-					}
-					else
-					{
-						take = -1;
-					}
+					//int take = taken? 1:-1;							
 					unsigned int vpca = bi.address;
 					unsigned int vghr = ghr;
 					btb_1->put(vpca,target);
-					p_pred->update(vpca,0,take,vghr);
+					p_pred->update(vpca,0,+1,vghr);
 				}
 			
 			}
 		}
 		if(bi.br_flags & BR_CONDITIONAL)
 		{
-			ghr <<= 1;
+			ghr <<= 1; //shift history
 			ghr |= taken;
 			ghr &= (1<<HISTORY_LENGTH)-1;
 		}	
